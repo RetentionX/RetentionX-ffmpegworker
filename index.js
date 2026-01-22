a0b2d62a749b5780e2f465e508307ab228191098
RetentionX <keshavmehta651@gmail.com>
2026-01-22 07:35:56 +0530

    Cors for frontend

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ const app = express();
 app.use(express.json());
 
 /* ===============================
+   CORS (REQUIRED FOR FRONTEND)
+================================ */
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+/* ===============================
    AUTH MIDDLEWARE (REQUIRED)
 ================================ */
 
